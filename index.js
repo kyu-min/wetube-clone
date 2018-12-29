142fc6b08daf40e5a00be0a66092404bb4b00d79
Kyu Min Lee <44955076+lestargem@users.noreply.github.com>
2018-12-29 16:31:27 +0900

    server

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ const express = require("express");
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+    console.log(req);
+    res.send("Hello from Home");
+}
+
+function handleProfile(req, res) {
+    res.send("You are on my Profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
