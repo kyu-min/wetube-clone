ec89c851b9d601eb2ed3932849d7c1ec750bc454
Kyu Min Lee <44955076+lestargem@users.noreply.github.com>
2018-12-31 23:23:31 +0900

    ES6

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import express from "express";
-const app = express();
- 
- const PORT = 4000;
+const app = express(); // var app = express();
+const PORT = 4000;  
  
  function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
+/* function handleListening() {
+  console.log("Listening on: http://localhost:" + PORT);
+} */
 
-const handleHome = (req, res) => {
+const handleHome = (req, res) => {     
     console.log(req);
     res.send("Hello from my home!");
 }
+/* var handleHome = function handleHome(req, res) {
+    console.log(req);
+    res.send("Hello from my home!");
+}; */
 
 const handleProfile = (req, res) => res.send("You are on my Profile");
+/* var handleProfile = function handleProfile(req, res) {
+  return res.send("You are on my Profile");
+}; */
 
 app.get("/", handleHome);
 
