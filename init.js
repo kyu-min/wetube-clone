3ea86bde51a137123b3337de40e428472b558085
Kyu Min Lee <44955076+lestargem@users.noreply.github.com>
2019-01-01 20:20:48 +0900

    initJS

---
+++ init.js
@@ -0,0 +1,38 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+const app = express(); // var app = express();
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+/* function handleListening() {
+  console.log("Listening on: http://localhost:" + PORT);
+} */
+
+const handleHome = (req, res) => {
+  console.log(req);
+  res.send("Hello from my home!");
+};
+/* var handleHome = function handleHome(req, res) {
+    console.log(req);
+    res.send("Hello from my home!");
+}; */
+
+const handleProfile = (req, res) => res.send("You are on my Profile");
+/* var handleProfile = function handleProfile(req, res) {
+  return res.send("You are on my Profile");
+}; */
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
