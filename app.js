24cfed83b6a5c846c1f82ae4f0e387cfc5c80b29
Kyu Min Lee <44955076+lestargem@users.noreply.github.com>
2019-01-14 03:01:50 +0900

    Router

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express"; // express를 1. 폴더 목록에서 먼저 찾고 2. 만약 목록에 없을시, node_modules에서 express를 찾아라는 명령
-import morgan from "morgan";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
+import morgan from "morgan"; // express 중, logging을 위한 기능.
+import helmet from "helmet"; // express 중, node.js의 보안을 위한 기능.
+import cookieParser from "cookie-parser"; // express 중, session을 다루기 위해 cookie에 유저 정보를 저장.
+import bodyParser from "body-parser"; //
 import { userRouter } from "./router";
 const app = express(); // var app = express(); 어플리케이션!
 
@@ -25,13 +25,34 @@ const handleProfile = (req, res) => res.send("You are on my Profile"); // 서버
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Route
-app.use(morgan("dev"));
-app.use(helmet());
+app.use(morgan("dev")); // morgan이 이 단계에서 모든 걸 기억한다.
+app.use(helmet()); // express의 한 기능이며, node.js의 보안을 위한 역할을 한다.
 
-app.get("/", handleHome); // Route
+app.get("/", handleHome); // Route, "/"가 Home임!
 
 app.get("/profile", handleProfile); // Route
 
 app.use("/user", userRouter); // Route
 
-export default app;
+export default app; // init.js에서 app.js를 사용하기 위해 app object를 export!
+
+/* middleware는 순서가 정말 중요하다.
+
+   쉽게 예제를 들자면, 클라이언트 쪽에서 로그인을 하거나 자료를 올리거나 할 때, 
+   완료되기 전에 중간에서 개발자인 내가 가로챌 수도 있고 관련 내용을 확인할 수도 있다.
+ 
+   더 쉽게 예제를 들어보자.
+   가령, IP주소를 체크하는 미들웨어를 만들었다고 가정하자.
+   만약 거부할 IP주소를 찾았다고 하면, 접속을 취소할 수 있겠지.
+ 
+   니꼴라스는 middleware를 betweenHome과 같은 변수를 통해서 실행하곤 했다.
+ 
+   루트가 반환되기 전에, 그 사이에 미들웨어를 넣는 게 순서에 맞다.
+   왜냐하면 코드를 에디터가 위에서 아래로 순서대로 읽어내려가기 때문이다.
+   말 그대로 가운데에 있는 소프트웨어라고 생각하자.
+   
+   Morgan은 logging에 도움을 주는 미들웨어다.
+   logging이란, 무슨 일이 어디에서 일어났는지를 기록하는 것이다.
+   설치는 npm install morgan을 통해 할 수 있다.
+   
+   helmet express는 node.js의 보안을 위한 것이다.*/
--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-export const userRouter = express.Router();
+export const userRouter = express.Router(); // userRouter를 내보낸다. Router로 Route들을 사용하기 위해.
 
 userRouter.get("/", (req, res) => res.send("user index"));
 userRouter.get("/edit", (req, res) => res.send("user edit"));
