72ed088c2cd2d71f2f834863b32208c4c94e801c
Kyu Min Lee <44955076+lestargem@users.noreply.github.com>
2019-01-14 01:38:07 +0900

    Route

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express"; // express를 1. 폴더 목록에서 먼저 찾고 2. 만약 목록에 없을시, node_modules에서 express를 찾아라는 명령
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
@@ -7,6 +7,7 @@ import { userRouter } from "./router";
 const app = express(); // var app = express();
 
 const handleHome = (req, res) => {
+  // 서버에서 Route에 응답
   console.log(req);
   res.send("Hello from my home!");
 };
@@ -15,7 +16,7 @@ const handleHome = (req, res) => {
     res.send("Hello from my home!");
 }; */
 
-const handleProfile = (req, res) => res.send("You are on my Profile");
+const handleProfile = (req, res) => res.send("You are on my Profile"); // 서버에서 Route에 응답
 /* var handleProfile = function handleProfile(req, res) {
   return res.send("You are on my Profile");
 }; */
@@ -26,9 +27,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(helmet());
 
-app.get("/", handleHome);
+app.get("/", handleHome); // Route
 
-app.get("/profile", handleProfile);
+app.get("/profile", handleProfile); // Route
 
 app.use("/user", userRouter);
 
